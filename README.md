52fb101799f669d5bef932e8027598a28d3c021e
manny <manny@manny>
2017-06-24 12:10:01 +0300

    Init project with bundles

---
+++ README.md
@@ -0,0 +1,4 @@
+iq
+==
+
+A Symfony project created on June 23, 2017, 10:50 pm.
---
+++ src/AppBundle/Entity/Currency.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Currency
+ *
+ * @ORM\Table(name="currency")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CurrencyRepository")
+ */
+class Currency
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=3, unique=true)
+     */
+    private $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="nominal", type="integer")
+     */
+    private $nominal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="value", type="decimal", precision=4, scale=4)
+     */
+    private $value;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Currency
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Currency
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set nominal
+     *
+     * @param integer $nominal
+     *
+     * @return Currency
+     */
+    public function setNominal($nominal)
+    {
+        $this->nominal = $nominal;
+
+        return $this;
+    }
+
+    /**
+     * Get nominal
+     *
+     * @return int
+     */
+    public function getNominal()
+    {
+        return $this->nominal;
+    }
+
+    /**
+     * Set value
+     *
+     * @param string $value
+     *
+     * @return Currency
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+}
+
